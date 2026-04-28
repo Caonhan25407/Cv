c69f2734f24db12627f80a8a1c876bb08fe9b597
Nhan <caotranthiennhan250407@gmail.com>
2026-04-28 18:46:59 +0700

    update

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
-gsap.to(".name-line", {
-  xPercent: -100,
-  repeat: -1,
-  duration: 15,
-  ease: "none"
-});
+let fullName = "Cao Tran Thien Nhan";
+let birthYear = 2007;
+let isStudent = true;
+
+const today = new Date();
+const currentYear = today.getFullYear();
+const age = currentYear - birthYear;
+
+console.log(`
+    Name: ${fullName}
+    Age: ${age}
+    Student: ${isStudent}
+    `);
+
+const tinhTrungBinh = (toan, van, anh) => {
+    return (toan + van + anh)/3;
+}
+
+const xepLoai = (diemTB) => {
+    if(diemTB >= 9) {
+        return "Xuat sac";
+    } else if(diemTB >=8 && diemTB < 9) {
+        return "Gioi";
+    } else if(diemTB >=7 && diemTB < 8) {
+        return "Kha";
+    } else if(diemTB >=6 && diemTB < 7) {
+        return "TB";
+    } else return "Yeu";
+}
+
+const diemToan = 10;
+const diemVan = 7;
+const diemAnh = 8;
+const myTB = tinhTrungBinh(diemToan, diemVan, diemAnh);
+
+console.log(`
+    DTB: ${myTB};
+    Xep loai: ${xepLoai(myTB)};
+    `);
